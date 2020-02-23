01fe8e90f9c31747d84d063f86ec5fe46223abca
jayjamie <jelexjam13@gmail.com>
2020-02-23 03:11:05 -0500

    Added LERP

---
+++ RevUC/Assets/ShrekWalk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrekWalk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject wayPoint;
+    private Vector3 wayPointPos;
+
+    private float speed = 6.0f;
+
+    void Start()
+    {
+        wayPoint = GameObject.Find("wayPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wayPointPos = new Vector3(wayPoint.transform.position.x, wayPoint.transform.position.y, wayPoint.transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, wayPointPos, speed * Time.deltaTime);
+    }
+}
