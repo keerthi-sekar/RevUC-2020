4a05ae0821a81de56cedddfc75d7348b8de7cc97
hydecj <hydecj@mail.uc.edu>
2020-02-22 19:29:41 -0500

    Create PlayerMovement.cs
    Movement

---
+++ RevUC/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+    public float speed = 12f;
+
+    public CharacterController controller;
+
+    public Transform groundCheck;
+
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    bool isGrounded;
+
+    public float gravity = -9.81f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
